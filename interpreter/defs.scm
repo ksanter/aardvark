ea15c64605adfec5ba6a43a0f9fed3abb9235a8e
nesanter <nesanter@knox.edu>
2014-06-11 10:57:53 -0500

    permutations, misc added

--- interpreter/defs.scm
+++ interpreter/defs.scm
@@ -7,9 +7,12 @@
 (define-record term-const binding range)
 (define-record term-var binding)
 (define-record term-op op operands)
+(define-record permutation forms body type)
 
 (define-record term-list l)
-(define-record expr type lh rh)
+(define-record expression type lh rh bindings plist)
+
+(define-record instance expr vals properties)
 
 (define try
   (lambda (t #!optional handle)
@@ -48,36 +51,30 @@
 
 (define constant
   (lambda (type #!optional binding range)
-    (begin
-      (aql-assert (num-type? type) "expected type as first argument to constant")
-      (if binding
-        (aql-assert (bound-term? binding) "expected binding or #f as second argument to constant")
-      )
-      (if range
-        (aql-assert (or (pair? range) (list? range)) "expected pair or list as third argument to constant")
-      )
-      (make-term-const binding range)
+    (aql-assert (num-type? type) "expected type as first argument to constant")
+    (if binding
+      (aql-assert (bound-term? binding) "expected binding or #f as second argument to constant")
     )
+    (if range
+      (aql-assert (or (pair? range) (list? range) (number? range)) "expected pair, list, or number as third argument to constant")
+    )
+    (make-term-const binding range)
   )
 )
 
 (define bind
   (lambda (ident)
-    (begin
-      (aql-assert (symbol? ident) "expected symbol as argument to bind")
-      (make-bound-term ident)
-    )
+    (aql-assert (symbol? ident) "expected symbol as argument to bind")
+    (make-bound-term ident)
   )
 )
 
 (define variable
   (lambda (#!optional binding)
-    (begin
-      (if binding
-        (aql-assert (bound-term? binding) "expected binding as argument to variable")
-      )
-      (make-term-var binding)
+    (if binding
+      (aql-assert (bound-term? binding) "expected binding as argument to variable")
     )
+    (make-term-var binding)
   )
 )
 
@@ -87,7 +84,7 @@
 
 (define term-object?
   (lambda (t)
-    (or (term-const? t) (term-var? t) (term-op? t))
+    (or (term-const? t) (term-var? t) (term-op? t) (permutation? t))
   )
 )
 
@@ -105,10 +102,8 @@
 
 (define terms
   (lambda (#!rest tlist)
-    (begin
-      (aql-assert (valid-tlist? tlist) "expected series of term objects as argument to terms")
-      (make-term-list tlist)
-    )
+    (aql-assert (valid-tlist? tlist) "expected series of term objects as argument to terms")
+    (make-term-list tlist)
   )
 )
 
@@ -162,14 +157,152 @@
   )
 )
 
-(define expression
-  (lambda (type lh rh)
-    (begin
-      (aql-assert (expr-type? type) "expected expression type as first argument to expression")
-      (aql-assert (and (term-list? lh) (term-list? rh)) "expected term list as second and third argumnet to expression")
-      (make-expr type lh rh)
+(define extract-terms
+  (lambda (t)
+    (cond
+      ((term-list? t) (term-list-l t))
+      ((term-op? t) (term-op-operands t))
+      ((permutation? t) (permutation-body t))
+      (else #f)
     )
   )
 )
 
+(define extract-bindings
+  (lambda (t)
+    (let ((initial (extract-terms t)))
+      (aql-assert initial "expected terms list or operator as first argument to extract-bindings")
+      (let loop ((tlist initial) (b '()))
+        (if (eqv? tlist '())
+          b
+          (let ((c (car tlist)))
+            (let ((b2
+                    (cond ((term-const? c) (term-const-binding c))
+                          ((term-var? c) (term-var-binding c))
+                          ((term-op? c) (let ((l (extract-bindings c)))
+                                          (if (eqv? l '())
+                                            #f
+                                            l
+                                          )
+                                        ))
+                          ((permutation? c) (let ((l (extract-bindings c)))
+                                              (if (eqv? l '())
+                                                #f
+                                                l
+                                              )
+                                            ))
+                   )
+                 ))
+              (loop (cdr tlist) (cons b2 b))
+            )
+          )
+        )
+      )
+    )
+  )
+)
 
+(define permutation-list
+  (lambda (t)
+    (let ((initial (extract-terms t)))
+      (aql-assert initial "expected terms list or operator as first argument to permutation-list")
+      (let loop ((p '()) (tlist initial))
+        (if (eqv? tlist '())
+          p
+          (let ((term (car tlist)))
+            (cond
+              ((permutation? term) (loop 
+                                     (let ((subpl (permutation-list term)))
+                                       (if (eqv? subpl '())
+                                         (cons (list (length (permutation-forms term))) p)
+                                         (cons (cons (length (permutation-forms term)) subpl) p)
+                                       )
+                                     )
+                                     (cdr tlist)
+                                    ))
+              ((term-op? term) (loop (cons (permutation-list term) p) (cdr tlist)))
+              (else (loop p (cdr tlist)))
+            )
+          )
+        )
+      )
+    )
+  )
+)
+
+(define expression-pnum
+  (lambda (expr)
+    (letrec ((flatten
+               (lambda (l)
+                 (cond
+                   ((null? l) '())
+                   ((pair? l) (append (flatten (car l)) (flatten (cdr l))))
+                   (else (list l))
+                 )
+               )
+            ))
+      (let ((flat (flatten (expression-plist expr))))
+        (apply * flat)
+      )
+    )
+  )
+)
+
+(define expression
+  (lambda (type lh rh)
+    (aql-assert (expr-type? type) "expected expression type as first argument to expression")
+    (aql-assert (and (term-list? lh) (term-list? rh)) "expected term list as second and third argumnet to expression")
+    (make-expression
+      type
+      lh
+      rh
+      (cons (extract-bindings lh) (extract-bindings rh))
+      (append (permutation-list lh) (permutation-list rh))
+    )
+  )
+)
+
+(define permute-type-maker
+  (lambda (type)
+    (lambda (#!rest forms)
+      (lambda (#!rest body)
+        (aql-assert (valid-tlist? body) "expect series of term objects as body of permutation")
+        (make-permutation forms body type)
+      )
+    )
+  )
+)
+
+(define random-permute (permute-type-maker 'random))
+(define permute (permute-type-maker 'straight))
+(define choose (permute-type-maker 'choice))
+
+;(define resolve-permutations
+;  (lambda (expr n)
+;    (let loop ((tlist (append (expression-lh expr) (expression-rh expr))))
+
+
+
+
+
+;(define new
+;  (lambda (expr #!optional count #!rest parameters)
+;    (let ((c (if (number? count) count 1)))
+;      (let loop ((n 0) (ies '()))
+;        (if (equal? n c)
+;          ies
+;          (let* (
+;                 (raw-expr (resolve-permutations expr n))
+;                 (gen-expr (gen raw-expr))
+;                )
+;            (loop
+;              (+ n 1)
+;              (cons (make-instance raw-expr gen-expr (map (lambda (name proc) (cons name (proc gen-expr))))) ies)
+;            )
+;          )
+;        )
+;      )
+;    )
+;  )
+;)
+;
